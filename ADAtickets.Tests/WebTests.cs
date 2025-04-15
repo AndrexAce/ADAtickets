405c55f85c6f261946f1038727bd997d7bb79dd2
Andrea Lucchese <andrylook14@gmail.com>
2025-04-15 18:14:49 +0200

    Suddivisione dei processi della GitHub Action e cambiamento libreria di test da MSBuild a xUnit 3.

--- ADAtickets.Tests/WebTests.cs
+++ ADAtickets.Tests/WebTests.cs
@@ -1,28 +1,40 @@
 namespace ADAtickets.Tests;
 
-[TestClass]
 public class WebTests
 {
-    [TestMethod]
-    public async Task GetWebResourceRootReturnsOkStatusCode()
+    public class IntegrationTest1
     {
-        // Arrange
-        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.ADAtickets_AppHost>();
-        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-        {
-            clientBuilder.AddStandardResilienceHandler();
-        });
+        // Instructions:
+        // 1. Add a project reference to the target AppHost project, e.g.:
+        //
+        //    <ItemGroup>
+        //        <ProjectReference Include="../MyAspireApp.AppHost/MyAspireApp.AppHost.csproj" />
+        //    </ItemGroup>
+        //
+        // 2. Uncomment the following example test and update 'Projects.MyAspireApp_AppHost' to match your AppHost project:
+        //
+        // [Fact]
+        // public async Task GetWebResourceRootReturnsOkStatusCode()
+        // {
+        //     // Arrange
+        //     var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.MyAspireApp_AppHost>();
+        //     appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
+        //     {
+        //         clientBuilder.AddStandardResilienceHandler();
+        //     });
+        //     // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
+        //
+        //     await using var app = await appHost.BuildAsync();
+        //     var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        //     await app.StartAsync();
 
-        await using var app = await appHost.BuildAsync();
-        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-        await app.StartAsync();
+        //     // Act
+        //     var httpClient = app.CreateHttpClient("webfrontend");
+        //     await resourceNotificationService.WaitForResourceAsync("webfrontend", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        //     var response = await httpClient.GetAsync("/");
 
-        // Act
-        var httpClient = app.CreateHttpClient("ADAticketsWeb");
-        await resourceNotificationService.WaitForResourceAsync("ADAticketsWeb", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-        var response = await httpClient.GetAsync("/");
-
-        // Assert
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        //     // Assert
+        //     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        // }
     }
 }
